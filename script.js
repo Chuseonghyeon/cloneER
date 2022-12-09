d6f8102d20d8d950c9ea755a88ae0305ece8c5ef
Chuseonghyeon <88300300+Chuseonghyeon@users.noreply.github.com>
2022-12-09 10:37:30 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function navigo (){
+    const header = document.querySelector('header'); 
+    const headerheight = header.clientHeight;
+  document.addEventListener('scroll', onScroll, { passive: true });
+   function onScroll () {
+       const scrollposition = pageYOffset;
+     const nav = document.querySelector('header')
+     if (headerheight<=scrollposition){
+       nav.classList.add('fix')
+     }
+     else {
+       nav.classList.remove('fix');
+     }
+   } 
+  }
+  navigo()
+
+function onHoverKakao()
+{
+    $("#kakao").attr('src', 'imgs/logo-kakao-whitegray.svg');
+}
+
+function offHoverKakao()
+{
+    $("#kakao").attr('src', 'imgs/logo-kakao-white.svg');
+}
+
+function onHoverErButton()
+{
+    $("#er-button").attr('class', "er-button");
+}
+
+function offHoverErButton()
+{
+    $("#er-button").attr('class', "");
+}
+
+function onHoverErButtonMain()
+{
+    $("#play-button").attr('class', "er-button");
+}
+
+function offHoverErButtonMain()
+{
+    $("#play-button").attr('class', "");
+}
